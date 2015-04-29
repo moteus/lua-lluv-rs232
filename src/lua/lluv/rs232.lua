3de7adfafdc858036bd637c227ced2de8c8cc232
Alexey Melnichuk <mimir@newmail.ru>
2015-04-29 18:18:31 +0400

    Add. Copyright comment.

--- src/lua/lluv/rs232.lua
+++ src/lua/lluv/rs232.lua
@@ -1,3 +1,14 @@
+------------------------------------------------------------------
+--
+--  Author: Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Copyright (C) 2014 Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Licensed according to the included 'LICENSE' document
+--
+--  This file is part of lua-lluv-rs232 library.
+--
+------------------------------------------------------------------
 local function zmq_device_poller(pipe, port_name, port_opt)
   pipe:set_linger(1000)
   pipe:set_sndtimeo(1000)
